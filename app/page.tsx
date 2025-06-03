2e160387f952c6980027e73d4f9032cb2187972f
oniafk <manufelipearias@gmail.com>
2025-06-04 07:33:58 +1000

    set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div>
+      <h1 className="text-2xl underline">Welcome to my recording app!</h1>
+      <Button>Let's get Statrted</Button>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
